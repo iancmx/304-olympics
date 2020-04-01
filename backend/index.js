1183f042d7442b282a54fb428ea46018f8189cea
Ming Xuan Choy <iancmx@hotmail.com>
2020-03-31 21:24:56 -0600

    attempt connection using oracledb

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mysql = require('mysql');
+const oracledb = require('oracledb');
+// const mysql = require('mysql');
 const userRoutes = require('./routes/user_routes');
 const dbRoutes = require('./routes/db_routes');
 const app = express();
 
-
-const db = mysql.createConnection({
-	host: 'dbhost.students.cs.ubc.ca:1522/stu',
-	user: 'ora_iancmx',
-	password: 'a74260878'
-})
-
-db.connect(function(err) {
-	if (err) {
-	  console.error('error connecting: ' + err.stack);
-	  return;
+try {
+	const db = oracledb.getConnection({
+		connectString: 'dbhost.students.cs.ubc.ca:1521:stu',
+		user: 'ora_iancmx',
+		password: 'a74260878'
+	})
+	console.log("Successfully connected to Oracle!")
+} catch (err) {
+	console.log("Error: ", err);
+} finally {
+	if (connection) {
+		try {
+		  await connection.close();
+		} catch(err) {
+		  console.log("Error when closing the database connection: ", err);
+		}
 	}
-   
-	console.log('connected as id ' + connection.threadId);
-  });
+}
+
 
 app.use(bodyParser.json());
 app.use('/user', userRoutes);
