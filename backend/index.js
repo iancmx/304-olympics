b738a91c7fcdb0eef0a5164d2d88a6d8166169fe
Ming Xuan Choy <iancmx@hotmail.com>
2020-03-31 18:26:17 -0600

    Added db connection

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mysql = require('mysql');
 const userRoutes = require('./routes/user_routes');
 const dbRoutes = require('./routes/db_routes');
 const app = express();
+
+
+const db = mysql.createConnection({
+	host: 'dbhost.students.cs.ubc.ca:1522/stu',
+	user: 'ora_iancmx',
+	password: 'a74260878'
+})
+
+db.connect(function(err) {
+	if (err) {
+	  console.error('error connecting: ' + err.stack);
+	  return;
+	}
+   
+	console.log('connected as id ' + connection.threadId);
+  });
 
 app.use(bodyParser.json());
 app.use('/user', userRoutes);
