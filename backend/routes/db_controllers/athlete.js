63da217f7b9f73773d0a32e213a3af3fbecc71d5
Ming Xuan Choy <iancmx@hotmail.com>
2020-04-03 16:02:25 -0600

    fix adding athlete route

--- backend/routes/db_controllers/athlete.js
+++ backend/routes/db_controllers/athlete.js
@@ -10,18 +10,40 @@ const newAthlete = async (req, res) => {
     gold_medal_count,
     silver_medal_count,
     bronze_medal_count,
-    participant_id
+    participant_id,
   } = req.body || {};
 
-  let insertQuery =
-    `INSERT INTO athlete (age, sex, weight, height, gold_medal_count, silver_medal_count, bronze_medal_count, participant_id) VALUES (${age}, '${sex}', ${weight}, ${height}, ${gold_medal_count}, ${silver_medal_count}, ${bronze_medal_count}, ${participant_id})`;
+  const sum =
+    parseInt(gold_medal_count) +
+    parseInt(silver_medal_count) +
+    parseInt(bronze_medal_count);
 
-  console.log(insertQuery);
+  await db.query(
+    `SELECT * FROM medalcount \
+  WHERE gold_medal_count = ${gold_medal_count} \
+AND silver_medal_count = ${silver_medal_count} \
+AND bronze_medal_count = ${bronze_medal_count} `,
+    async (err, result) => {
+      if (err) throw err;
+      if (!result[0]) {
+        let insertMCquery = `INSERT INTO medalcount VALUES (${gold_medal_count}, ${silver_medal_count}, ${bronze_medal_count}, ${sum})`;
+        console.log(insertMCquery);
+        await db.query(insertMCquery, (err, res) => {
+          if (err) throw err;
+        });
+      }
+      let insertQuery = `INSERT INTO athlete (age, sex, weight, height, gold_medal_count, silver_medal_count, bronze_medal_count, participant_id) VALUES (${age}, '${sex}', ${weight}, ${height}, ${gold_medal_count}, ${silver_medal_count}, ${bronze_medal_count}, ${participant_id})`;
 
-  db.query(insertQuery, (err, res) => {
-    if (err) throw err;
-    res.json(res.insertId);
-  });
+      console.log(insertQuery);
+    
+      db.query(insertQuery, (err, resp) => {
+        if (err) throw err;
+        res.json(resp.insertId);
+      });
+    }
+  );
+
+
 };
 
 const updateAthlete = async (req, res) => {};
@@ -38,10 +60,13 @@ const allAthletes = async (req, res) => {
 };
 
 const numAthleteAgeCountry = async (req, res) => {
-  db.query("SELECT P.country, COUNT(*) AS number_of_athletes, AVG(A.age) FROM athlete A, participant P WHERE A.participant_id = P.participant_id  GROUP BY P.country;", (err, result, fields) => {
-    if (err) throw err;
-    res.json(result);
-  });
+  db.query(
+    "SELECT P.country, COUNT(*) AS number_of_athletes, AVG(A.age) FROM athlete A, participant P WHERE A.participant_id = P.participant_id  GROUP BY P.country;",
+    (err, result, fields) => {
+      if (err) throw err;
+      res.json(result);
+    }
+  );
 };
 
 module.exports = {
@@ -50,5 +75,5 @@ module.exports = {
   deleteAthlete,
   getInfo,
   allAthletes,
-  numAthleteAgeCountry
+  numAthleteAgeCountry,
 };
