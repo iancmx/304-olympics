13adddf99d7c45f9b84e83415fcdd88632cdf89e
Yash_Sardhara <yash.sardhara1@gmail.com>
2020-04-03 02:52:13 -0700

    Login Update

--- frontend/olympic-website/src/Components/Login/login.jsx
+++ frontend/olympic-website/src/Components/Login/login.jsx
@@ -5,6 +5,34 @@ export class Login extends React.Component  {
 
 	constructor(props) {
 		super(props);
+		this.state = {
+			username: "",
+			password: ""
+		}
+
+		this.usernameChange = this.usernameChange.bind(this);
+		this.passwordChange = this.passwordChange.bind(this);
+	}
+
+	usernameChange(event){
+		this.setState({username: event.target.value,});
+	}
+
+	passwordChange(event){
+		this.setState({password: event.target.value,});
+	}
+
+	handleSubmit(){
+		const username = this.state.username;
+		const password = this.state.password;
+
+		if (!username) {
+			alert("Please enter your username!");
+		} else if (!password) {
+			alert("Please enter your password!");
+		} else {
+			alert("Hello " + username + "! Your passoword is " + password+ "!");
+		}
 	}
 
 	render() {
@@ -18,16 +46,16 @@ export class Login extends React.Component  {
 				<div className="form">
 					<div className="form-group">
 						<label htmlFor="username">Username</label>
-						<input type="text" name="username" placeholder="username"/> 
+						<input type="text" name="username" placeholder="username" onChange={this.usernameChange}/> 
 					</div>
 					<div className="form-group">
 						<label htmlFor="password">Password</label>
-						<input type="password" name="password" placeholder="password"/> 
+						<input type="password" name="password" placeholder="password" onChange={this.passwordChange}/> 
 					</div>
 				</div>
 			</div> 
 			<div className="footer">
-				<button type="button" className="btn">Login</button>
+				<button type="button" className="btn" onClick={() => this.handleSubmit()}>Login</button>
 			</div>
 		</div>
 		)
--- frontend/olympic-website/src/Components/Login/register.jsx
+++ frontend/olympic-website/src/Components/Login/register.jsx
@@ -5,6 +5,44 @@ export class Register extends React.Component  {
 
 	constructor(props) {
 		super(props);
+		this.state = {
+			username: "",
+			password: "",
+			confirmPassword: ""
+		}
+
+		this.usernameChange = this.usernameChange.bind(this);
+		this.passwordChange = this.passwordChange.bind(this);
+		this.confirmPasswordChange = this.confirmPasswordChange.bind(this);
+	}
+
+	usernameChange(event){
+		this.setState({username: event.target.value});
+	}
+
+	passwordChange(event){
+		this.setState({password: event.target.value});
+	}
+
+	confirmPasswordChange(event){
+		this.setState({confirmPassword: event.target.value});
+	}
+	handleSubmit(){
+		const username = this.state.username;
+		const password = this.state.password;
+		const confirmPassword = this.state.confirmPassword;
+
+		if (!username) {
+			alert("Please enter your username!");
+		} else if (!password) {
+			alert("Please enter your password!");
+		} else if (!confirmPassword) {
+			alert("Please confirm your password!");
+		} else if (password != confirmPassword) {
+			alert("Your password confirmation does not match!");
+		} else {
+			alert("Hello " + username + "! Your passoword is " + password+ "!");
+		}
 	}
 
 	render() {
@@ -18,20 +56,20 @@ export class Register extends React.Component  {
 				<div className="form">
 					<div className="form-group">
 						<label htmlFor="username">Username</label>
-						<input type="text" name="username" placeholder="username"/> 
+						<input type="text" name="username" placeholder="username"  onChange={this.usernameChange}/> 
 					</div>
 					<div className="form-group">
 						<label htmlFor="password">Password</label>
-						<input type="password" name="password" placeholder="password"/> 
+						<input type="password" name="password" placeholder="password"  onChange={this.passwordChange}/> 
 					</div>
 					<div className="form-group">
 						<label htmlFor="confirm password">Confirm Password</label>
-						<input type="password" name="confirm password" placeholder="confirm password"/> 
+						<input type="password" name="confirm password" placeholder="confirm password" onChange={this.confirmPasswordChange}/> 
 					</div>
 				</div>
 			</div> 
 			<div className="footer">
-				<button type="button" className="btn">Register</button>
+				<button type="button" className="btn" onClick={() => this.handleSubmit()}>Register</button>
 			</div>
 		</div>
 		)
