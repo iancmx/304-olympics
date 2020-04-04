6ee9c1777ab5b5db75c7da719c2bed9cf1b955a8
Ming Xuan Choy <iancmx@hotmail.com>
2020-04-04 16:38:59 -0600

    restored athlete to original file

--- backend/routes/db_controllers/athlete.js
+++ backend/routes/db_controllers/athlete.js
@@ -33,11 +33,57 @@ AND bronze_medal_count = ${bronze_medal_count} `,
           if (err) throw err;
         });
       }
-      let insertQuery = `INSERT INTO athlete (age, sex, weight, height, gold_medal_count, silver_medal_count, bronze_medal_count, participant_id) VALUES (${age}, "${sex}", ${weight}, ${height}, ${gold_medal_count}, ${silver_medal_count}, ${bronze_medal_count}, ${participant_id})`;
 
-      console.log(insertQuery);
+      let insertQuery1 = `INSERT INTO participant (name, country) VALUES ('${name}', '${country}')`;
+      db.query(insertQuery1, (err, result) => {
+        if (err) throw err;
+        let insertQuery2 = `INSERT INTO athlete (age, sex, weight, height, gold_medal_count, silver_medal_count, bronze_medal_count, participant_id) VALUES (${age}, "${sex}", ${weight}, ${height}, ${gold_medal_count}, ${silver_medal_count}, ${bronze_medal_count}, ${result.insertId})`;
+        db.query(insertQuery2, (err, resp) => {
+          if (err) throw err;
+          res.json(resp.insertId);
+        });
+      });
+    }
+  );
+};
 
-      db.query(insertQuery, (err, resp) => {
+const updateAthlete = async (req, res) => {
+  const {
+    athlete_id,
+    age,
+    sex,
+    weight,
+    height,
+    gold_medal_count,
+    silver_medal_count,
+    bronze_medal_count,
+    participant_id,
+  } = req.body || {};
+
+  const sum =
+    parseInt(gold_medal_count) +
+    parseInt(silver_medal_count) +
+    parseInt(bronze_medal_count);
+
+  await db.query(
+    `SELECT * FROM medalcount \
+  WHERE gold_medal_count = ${gold_medal_count} \
+AND silver_medal_count = ${silver_medal_count} \
+AND bronze_medal_count = ${bronze_medal_count} `,
+    async (err, result) => {
+      if (err) throw err;
+      if (!result[0]) {
+        let insertMCquery = `INSERT INTO medalcount VALUES (${gold_medal_count}, ${silver_medal_count}, ${bronze_medal_count}, ${sum})`;
+        console.log(insertMCquery);
+        await db.query(insertMCquery, (err, res) => {
+          if (err) throw err;
+        });
+      }
+      let updateQuery = `UPDATE athlete SET age = ${age}, sex = "${sex}", weight = ${weight}, height = ${height}, gold_medal_count = ${gold_medal_count}, silver_medal_count = ${silver_medal_count}, bronze_medal_count = ${bronze_medal_count} WHERE athlete_id = ${athlete_id}`;
+
+      console.log(updateQuery);
+
+      db.query(updateQuery, (err, resp) => {
         if (err) throw err;
         res.json(resp.insertId);
       });
@@ -45,11 +91,25 @@ AND bronze_medal_count = ${bronze_medal_count} `,
   );
 };
 
-const updateAthlete = async (req, res) => {};
+const deleteAthlete = async (req, res) => {
+  db.query(
+    `DELETE FROM athlete WHERE athlete_id = ${req.params.id}`,
+    (err, result, fields) => {
+      if (err) throw err;
+      res.json(result);
+    }
+  );
+};
 
-const deleteAthlete = async (req, res) => {};
-
-const getInfo = async (req, res) => {};
+const getInfo = async (req, res) => {
+  db.query(
+    `SELECT * FROM athlete WHERE athlete_id = ${req.params.id}`,
+    (err, result, fields) => {
+      if (err) throw err;
+      res.json(result);
+    }
+  );
+};
 
 const allAthletes = async (req, res) => {
   db.query("SELECT * FROM athlete", (err, result, fields) => {
@@ -60,7 +120,7 @@ const allAthletes = async (req, res) => {
 
 const numAthleteAgeCountry = async (req, res) => {
   db.query(
-    "SELECT P.country, COUNT(*) AS number_of_athletes, AVG(A.age) FROM athlete A, participant P WHERE A.participant_id = P.participant_id  GROUP BY P.country;",
+    "SELECT P.country AS country, COUNT(*) AS number_of_athletes, AVG(A.age) AS average FROM athlete A, participant P WHERE A.participant_id = P.participant_id  GROUP BY P.country",
     (err, result, fields) => {
       if (err) throw err;
       res.json(result);
@@ -74,5 +134,5 @@ module.exports = {
   deleteAthlete,
   getInfo,
   allAthletes,
-  numAthleteAgeCountry,
+  numAthleteAgeCountry
 };
