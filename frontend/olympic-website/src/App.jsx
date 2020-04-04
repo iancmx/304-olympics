f44eb2122f98fbeaf123cfbb9499a6f1c29fea26
khatung <tungdjack@gmail.com>
2020-04-03 21:36:38 -0700

    404

--- frontend/olympic-website/src/App.jsx
+++ frontend/olympic-website/src/App.jsx
@@ -1,25 +1,27 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import "./App.scss";
-import Home from "./Components/Home"
+import Home from "./Components/Home";
+import notFoundPage from "./Components/404";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-	constructor(props) {
-		super(props);
-	}
-
-	render() {
-		return (
-			<div className="App">
-				<BrowserRouter>
-					<Switch>
-						<Route exact path={"/"} component = { Home } />
-					</Switch>
-				</BrowserRouter>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <Switch>
+            <Route exact path={"/"} component={Home} />
+            <Route exact parth={"/404"} component={notFoundPage} />
+            <Redirect to="/404" />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
